5341bab0b9fccc7b86094b455225230a0ffdf028
George Gastaldi <gegastaldi@gmail.com>
2015-01-16 17:30:16 -0200

    Improved javadoc

--- ui/src/main/java/org/jboss/forge/netbeans/ui/wizard/RunForgeWizardRunnable.java
+++ ui/src/main/java/org/jboss/forge/netbeans/ui/wizard/RunForgeWizardRunnable.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.jboss.forge.netbeans.ui.wizard;
 
 import java.text.MessageFormat;
@@ -22,6 +17,7 @@ import org.openide.WizardDescriptor;
 import org.openide.util.Exceptions;
 
 /**
+ * A Runnable suited to run a Forge command
  *
  * @author <a href="mailto:ggastald@redhat.com">George Gastaldi</a>
  */
