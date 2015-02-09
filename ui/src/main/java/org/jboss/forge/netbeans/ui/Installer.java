c6a61c75fa6dffc40aae17ad45974e1091b489d0
George Gastaldi <gegastaldi@gmail.com>
2015-02-09 12:11:12 -0200

    Changed License Header

--- ui/src/main/java/org/jboss/forge/netbeans/ui/Installer.java
+++ ui/src/main/java/org/jboss/forge/netbeans/ui/Installer.java
@@ -1,7 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/* 
+ * Copyright (c) 2015 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    <a href="mailto:ggastald@redhat.com">George Gastaldi</a> - initial API and implementation and/or initial documentation
  */
 package org.jboss.forge.netbeans.ui;
 
--- ui/src/main/java/org/jboss/forge/netbeans/ui/wizard/component/ComponentBuilderRegistry.java
+++ ui/src/main/java/org/jboss/forge/netbeans/ui/wizard/component/ComponentBuilderRegistry.java
@@ -1,8 +1,11 @@
-/*
- * Copyright 2014 Red Hat, Inc. and/or its affiliates.
+/* 
+ * Copyright (c) 2015 Red Hat, Inc. and/or its affiliates.
  *
  * Licensed under the Eclipse Public License version 1.0, available at
  * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    <a href="mailto:ggastald@redhat.com">George Gastaldi</a> - initial API and implementation and/or initial documentation
  */
 package org.jboss.forge.netbeans.ui.wizard.component;
 
